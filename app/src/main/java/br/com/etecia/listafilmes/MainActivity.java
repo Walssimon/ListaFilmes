1ecf5387734fb5bdde1bb8b3bb305ed8b5449c3d
Walssimon <walssimon123@gmail.com>
2022-12-01 20:22:19 -0300

    Atualizacao

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -1,14 +1,46 @@
 package br.com.etecia.listafilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listaFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaFilmes = findViewById(R.id.idlistaFilmes);
+        MyAdapter adapter = new MyAdapter();
+
+
+        listaFilmes.setAdapter(adapter);
+    }
+    //Criando uma inner class MyAdapter
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
